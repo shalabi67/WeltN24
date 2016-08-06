37ff4ccebefdfc4b464beb4b1e31c38c1b508d77
mohammad <shalabimobile@gmail.com>
2016-08-06 23:46:54 +0200

    async is working

---
+++ WeltN24/Readme.txt
@@ -0,0 +1,13 @@
+WeltN24
+
+"Asynchronous API Gateway".
+
+Your task is to write a short example app: An API gateway (preferable Spring/Playframwork) that proxies two different REST end points and outputs the results into a merged (also JSON) response.
+
+For example you could use these two endpoints:
+1.  http://jsonplaceholder.typicode.com/users/1 to obtain a user's data
+
+
+2.  http://jsonplaceholder.typicode.com/posts?userId=1 to obtain all comments written by that user
+
+
---
+++ WeltN24/src/main/java/com/weltn24/async/UserTask.java
@@ -0,0 +1,40 @@
+package com.weltn24.async;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import com.weltn24.user.User;
+
+
+public class UserTask implements Runnable  {
+	private String serviceUrl;
+	private DeferredResult<User> result;
+	public UserTask(String serviceUrl, DeferredResult<User> result) {
+		this.serviceUrl = serviceUrl;
+		this.result = result;
+	}
+
+	@Override
+	public void run() {
+		RestTemplate restTemplate = new RestTemplate();
+  		User user = new User();
+  		try {
+  			ResponseEntity<User> userEntity = restTemplate.exchange(serviceUrl, 
+  					HttpMethod.GET, 
+  					new HttpEntity<String>(HttpHeader.getHttpHeaders()), 
+  					User.class);
+  			
+  			user = userEntity.getBody();
+  		}
+  		catch(RestClientException e) {
+  			e.printStackTrace();
+  		} 
+  		result.setResult(user);
+		
+	}
+
+}
