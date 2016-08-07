907442a51d0b756c3068fa4dd7713318e8341170
mohammad <shalabimobile@gmail.com>
2016-08-07 13:38:29 +0200

    adding gitignore

---
+++ WeltN24/src/main/java/com/weltn24/async/UserPostsTask.java
@@ -0,0 +1,40 @@
+package com.weltn24.async;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import com.weltn24.user.Post;
+
+
+public class UserPostsTask implements Runnable  {
+	private String serviceUrl;
+	private DeferredResult<Post[]> result;
+	public UserPostsTask(String serviceUrl, DeferredResult<Post[]> result) {
+		this.serviceUrl = serviceUrl;
+		this.result = result;
+	}
+
+	@Override
+	public void run() {
+		RestTemplate restTemplate = new RestTemplate();
+  		Post[] userPosts = new Post[0];
+  		try {
+  			ResponseEntity<Post[]> userEntity = restTemplate.exchange(serviceUrl, 
+  					HttpMethod.GET, 
+  					new HttpEntity<String>(HttpHeader.getHttpHeaders()), 
+  					Post[].class);
+  			
+  			userPosts = userEntity.getBody();
+  		}
+  		catch(RestClientException e) {
+  			e.printStackTrace();
+  		} 
+  		result.setResult(userPosts);
+		
+	}
+
+}
